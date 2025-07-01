8ecf06331e39728d825d7cab7a88bb73147a1efb
pai0id <andrey.polyakov05@gmail.com>
2025-07-01 18:06:33 +0300

    controller init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HierarchyController } from './hierarchy/hierarchy.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HierarchyController],
   providers: [AppService],
 })
 export class AppModule {}
