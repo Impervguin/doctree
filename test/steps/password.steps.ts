672f316781e6759759e582aaedf4397b7495b749
impervguin <leon53000@yandex.ru>
2025-10-11 02:11:07 +0300

    ci/cd upd

--- test/steps/password.steps.ts
+++ test/steps/password.steps.ts
@@ -116,6 +116,7 @@ defineFeature(feature, (test) => {
       });
   
       and('I retrieve the reset token from email', async () => {
+        await new Promise(r => setTimeout(r, 10000));
         resetToken = await userManager.getResetPasswordTokenFromEmail(userEmail);
         expect(resetToken).toMatch(/^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.[A-Za-z0-9-_.+/=]*$/);
       });
@@ -188,6 +189,7 @@ defineFeature(feature, (test) => {
     });
 
     and('I retrieve the reset token from email', async () => {
+      await new Promise(r => setTimeout(r, 10000));
       resetToken = await userManager.getResetPasswordTokenFromEmail(userEmail);
       expect(resetToken).toMatch(/^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.[A-Za-z0-9-_.+/=]*$/);
     });
