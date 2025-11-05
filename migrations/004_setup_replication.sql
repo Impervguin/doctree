fe71b012a82ac1fd2c6ac357f7f7dd31facd7ece
impervguin <leon53000@yandex.ru>
2025-11-05 22:34:42 +0300

    done task5

--- migrations/004_setup_replication.sql
+++ migrations/004_setup_replication.sql
@@ -6,9 +6,13 @@ CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_pass';
 
 -- Grant necessary permissions
 GRANT ALL PRIVILEGES ON DATABASE doctree TO replicator;
+GRANT USAGE ON SCHEMA public TO replicator;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO replicator;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO replicator;
 
 -- Create publication for all tables
-CREATE PUBLICATION doctree_pub FOR ALL TABLES;
+CREATE PUBLICATION doctree_pub FOR TABLE
+app_admin, app_user, document_relations, documents, documents_files, documents_nodes, documents_tags, files, node_closure, nodes, parse_schedulers, parsed_files;
 
 -- +goose StatementEnd
 
